1987f87767cfc02a5d1a25388edcc515f158fdf6
Kevin FERNANDO <kfernando-prestataire@seres.fr>
2023-06-22 09:57:13 +0200

    quasar ajax bar demo, ran prettier

---
+++ src/components/AjaxBarDemo.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-ajax-bar ref="ajaxBar" color="accent" size="10px" skip-hijack />
+  <button @click="triggerAjaxBar">trigger ajaxbar</button>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    const ajaxBar = ref(null);
+
+    const triggerAjaxBar = () => {
+      const ajaxBarRef = ajaxBar.value;
+      ajaxBarRef.start();
+
+      setTimeout(() => {
+        if (ajaxBarRef) {
+          ajaxBarRef.stop();
+        }
+      }, 5000);
+    };
+
+    return {
+      ajaxBar,
+      triggerAjaxBar,
+    };
+  },
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center" style="flex-direction: column">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
+    <AjaxBarDemo />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+
+import AjaxBarDemo from "src/components/AjaxBarDemo.vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  components: { AjaxBarDemo },
+});
 </script>
